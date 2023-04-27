490b5ad3fb0175c54d1956f30c44d45b9ae837ec
Fredoly W <starkwagni@gmail.com>
2023-04-27 15:46:54 +0000

    Update 5 files

--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -3,12 +3,12 @@
 		class="text-center md:text-left font-bold text-slate-200 text-4xl md:text-5xl mb-8 md:mb-16 w-full"
 	>
 		Tailored recommendations<br class="hidden md:block" /> for TV shows and films
-		<br class="hidden md:block" /> using Open AI
+		<br class="hidden md:block" /> using ChatGPT
 	</div>
 	<div class="flex flex-wrap items-center mr-auto">
 		<button
 			on:click
-			class="w-full md:w-auto text-center bg-pink-600 hover:bg-pink-700 mb-4 flex-none rounded-full text-white font-medium py-3 px-6 mr-8"
+			class="w-full md:w-auto text-center bg-red-600 hover:bg-red-700 mb-4 flex-none rounded-full text-white font-medium py-3 px-6 mr-8"
 		>
 			Get recommendations
 		</button>
